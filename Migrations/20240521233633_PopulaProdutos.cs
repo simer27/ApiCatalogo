5d82469dd5d3bae4dfcb50c78dd1a9e996a8b646
Simeao Almeida <simer27@yahoo.com>
2024-05-21 21:05:50 -0300

    CRIADO UM ARQUIVO MIGRATION VAZIO, POR NOME PopulaProdutos, DEPOIS CRIEI OS COMANDOS DE INSERT E DELETE E RODEI O UPDATE NO BANCO DE DADOS

---
+++ Migrations/20240521233633_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into produtos(Nome, Descricao, Preco, imageUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Suco de Laranja','Suco de Laranja 500ml',7.45,'sucolaranja.jpg',10,now(),1)");
+            mb.Sql("Insert into produtos(Nome, Descricao, Preco, imageUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Lanche de Atum','Lanche de Atum com Maionese',7.99,'lancheatum.jpg',10,now(),2)");
+            mb.Sql("Insert into produtos(Nome, Descricao, Preco, imageUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Pudim 100g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
