60adbe9c6f1a5fcc427dd11e5aa739fb25bd2cdc
Simeao Almeida <simer27@yahoo.com>
2024-05-23 17:15:17 -0300

    corrigindo a Serealização do Json para POST E PUT de Categoria e Produto

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ApiCatalogo.Models;
 
@@ -19,5 +20,6 @@ public class Categoria
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
+    [JsonIgnore]
     public ICollection<Produto>? Produtos{ get; set; }
 }
