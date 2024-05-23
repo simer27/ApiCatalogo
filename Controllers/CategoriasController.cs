67c13ac3cdcd17c7cfcbbda438cc661c2dc24496
Simeao Almeida <simer27@yahoo.com>
2024-05-23 15:04:54 -0300

    criação do arquivo CategoriasController.cs com o método GET

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,29 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _context.Categorias.ToList();
+            if (categorias is null)
+            {
+                return NotFound("Categotias não encontradas...");
+            }
+
+            return Ok(categorias);
+        }
+    }
+}
